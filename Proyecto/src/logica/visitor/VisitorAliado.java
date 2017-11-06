302dcbc91a8da349d8a7476df734aaef088e8535
JuanmaFloyd <juanma_giron@hotmail.es>
2017-11-06 10:53:27 -0300

    visitoraliado fix

--- Proyecto/src/logica/visitor/VisitorAliado.java
+++ Proyecto/src/logica/visitor/VisitorAliado.java
@@ -4,7 +4,6 @@ import logica.Tile;
 import objeto.noAtravesable.ObjectStateStatic;
 import objeto.noAtravesable.objetoConVida.OMConVida.*;
 import objeto.noAtravesable.objetoConVida.Premios.Premio;
-import objeto.noAtravesable.objetoConVida.personaje.*;
 import objeto.noAtravesable.objetoConVida.personaje.aliado.Aliado;
 import objeto.noAtravesable.objetoConVida.personaje.enemigo.Enemigo;
 
@@ -19,8 +18,8 @@ public class VisitorAliado extends Visitor {
 	//metodos
 	public void afectar(Aliado a) {}
 	public void afectar(Enemigo e) {
-		e.restarVida(aliado.getImpacto());
 		aliado.setState(aliado.getAttackingState());
+		e.restarVida(aliado.getImpacto());
 	}
 	public void afectar(OMConVida omcv) {
 		int pos = aliado.getTile().getColumna();
@@ -37,5 +36,19 @@ public class VisitorAliado extends Visitor {
 		if (!encontre)	
 			aliado.setState(new ObjectStateStatic());
 	}
-	public void afectar(Premio p) {}
+	public void afectar(Premio p) {
+		int pos = aliado.getTile().getColumna();
+		int act = p.getTile().getColumna();
+		Tile current = p.getTile().getLeft();
+		boolean encontre = false;
+		for (int i = pos-act; !encontre && i<aliado.getAlcance(); i++){
+			if (current.getComponente()!=null){
+				aliado.atacar(current.getComponente());
+				encontre = true;
+			}
+			current = current.getLeft();
+		}
+		if (!encontre)	
+			aliado.setState(new ObjectStateStatic());
+	}
 }
