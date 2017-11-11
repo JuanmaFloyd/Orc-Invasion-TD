2ca94519d5513e1c8ced42665d0a3c7657a4c145
JuanmaFloyd <juanma_giron@hotmail.es>
2017-11-10 21:24:23 -0300

    lava visitor

--- Proyecto/src/logica/visitor/VisitorLava.java
+++ Proyecto/src/logica/visitor/VisitorLava.java
@@ -1,5 +1,6 @@
 package logica.visitor;
 
+import logica.Logica;
 import objeto.atravesable.Lava;
 import objeto.noAtravesable.objetoConVida.OMConVida.OMConVida;
 import objeto.noAtravesable.objetoConVida.Premios.Premio;
@@ -9,16 +10,22 @@ import objeto.noAtravesable.objetoConVida.personaje.enemigo.Enemigo;
 public class VisitorLava extends Visitor {
 	//atributos
 	protected Lava lava;
+	protected int n;
 	
 	//constructor
 	public VisitorLava(Lava l){
 		lava=l;
+		n = 0;
 	}
 	public void afectar(Aliado a) {
-		a.restarVida(lava.getImpacto());
+		if (n % 10 == 0)
+			a.restarVida(lava.getImpacto());
+		n = n+1;
 	}
 	public void afectar(Enemigo e) {
-		e.restarVida(lava.getImpacto());
+		if (n % 10 == 0)	
+			e.restarVida(lava.getImpacto());
+		n = n+1;
 	}
 	public void afectar(OMConVida omcv) {
 		omcv.restarVida(lava.getImpacto());
