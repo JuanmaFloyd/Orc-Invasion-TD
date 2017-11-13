f4f8f43f652a0a53d1e30082420bf13549de3025
FacundoMarin <facu1803@yahoo.com.ar>
2017-11-12 22:53:53 -0300

    Couple of anims

--- Proyecto/src/logica/visitor/VisitorClerigo.java
+++ Proyecto/src/logica/visitor/VisitorClerigo.java
@@ -1,19 +1,22 @@
 package logica.visitor;
 
-import objeto.noAtravesable.objetoConVida.OMConVida.OMConVida;
-import objeto.noAtravesable.objetoConVida.Premios.Premio;
+
 import objeto.noAtravesable.objetoConVida.personaje.aliado.Aliado;
 import objeto.noAtravesable.objetoConVida.personaje.aliado.Clerigo;
-import objeto.noAtravesable.objetoConVida.personaje.enemigo.Enemigo;
+
 
 public class VisitorClerigo extends VisitorAliado{
 	public VisitorClerigo(Clerigo a){
 		super(a);
 	}
-	public void afectar(Aliado a) {a.sumarVida(5);}
-	public void afectar(Enemigo e) {
-		e.restarVida(aliado.getImpacto());
+	public void afectar(Aliado a) {
+		if (a.getVida() < a.getMaxVida()){
+			a.sumarVida(5);
+			aliado.setState(aliado.getAttackingState());
+		}
+		else{
+			aliado.setState(aliado.getStaticState());
+		}
 	}
-	public void afectar(OMConVida omcv) {}
-	public void afectar(Premio p) {}
 }
+
