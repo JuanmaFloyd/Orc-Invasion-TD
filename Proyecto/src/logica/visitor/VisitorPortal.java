4e2d26fa5b5793cd636b26104f3149825ed6c59b
JuanmaFloyd <juanma_giron@hotmail.es>
2017-11-17 18:07:38 -0300

    portal kind of temporal

--- Proyecto/src/logica/visitor/VisitorPortal.java
+++ Proyecto/src/logica/visitor/VisitorPortal.java
@@ -1,5 +1,6 @@
 package logica.visitor;
 
+import logica.Logica;
 import objeto.atravesable.PortalIn;
 import objeto.atravesable.PortalOut;
 import objeto.noAtravesable.objetoConVida.OMConVida.OMConVida;
@@ -10,10 +11,12 @@ import objeto.noAtravesable.objetoConVida.personaje.enemigo.Enemigo;
 public class VisitorPortal extends Visitor{
 	protected PortalOut po;
 	protected PortalIn pi;
+	protected int cant;
 	
 	public VisitorPortal(PortalOut o, PortalIn i){
 		po = o;
 		pi = i;
+		cant = 0;
 	}
 
 	public void afectar(Aliado a) {}
@@ -22,6 +25,17 @@ public class VisitorPortal extends Visitor{
 			pi.getTile().setComponente(null);
 			po.getTile().setComponente(e);
 			e.setTile(po.getTile());
+			cant++;
+		}
+		
+		if (cant == 5){
+			Logica.getLogica().agregarAtravesableABorrar(pi);
+			pi.setVP(null);
+			Logica.getLogica().agregarAtravesableABorrar(po);
+			pi.getTile().setComponenteAtravesable(null);
+			po.getTile().setComponenteAtravesable(null);
+			pi.setTile(null);
+			po.setTile(null);
 		}
 	}
 	public void afectar(OMConVida omcv) {}
